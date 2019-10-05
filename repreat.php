aeb2bff0eaaba7fab0dd5723d51f6570b3476abb
Parid Dudin <52320684+mr-parid@users.noreply.github.com>
2019-10-05 14:45:08 +0700

    Add files via upload

---
+++ repreat.php
@@ -0,0 +1,22 @@
+<?php
+//PEMBELAJARAN REPEAT
+
+//Dibawah Ini Contoh For
+for ($i=0; $i <5; $i++) {
+    echo "Parid Dudin <br>";
+}
+
+//Dibawah Ini Contoh While
+$i=0;
+while ($i<=5) {
+    echo "Parid Dudin <br>";
+    $i++;
+}
+
+//Dibawah Ini Contoh Do While
+$i=0;
+do {
+    echo "Parid Dudin <br>";
+    $i++;
+} while ($i<=7);
+?>
