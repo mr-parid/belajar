587571e42d318d77604a076579614f89aff4260e
Parid Dudin <52320684+mr-parid@users.noreply.github.com>
2019-10-05 14:49:09 +0700

    Add files via upload

---
+++ fortable3.php
@@ -0,0 +1,12 @@
+<?php 
+//Pengkondisian
+echo "Ini adalah angka kurang dari 20, maka hasilnya adalah ";
+$x=10;
+if($x<20) {
+    echo "Benar";
+} else if ($x == 20) {
+    echo "Masih Sama";
+} else {
+    echo "Salah";
+}
+?>
